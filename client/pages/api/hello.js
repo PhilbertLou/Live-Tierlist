51998051c9d46b5ccc36208ed9e85fbfdbfe1148
PhilbertLou <philbert.lou@uwaterloo.ca>
2021-02-13 21:22:24 -0500

    ensured we can call other functions

--- client/pages/api/hello.js
+++ client/pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import addTier from '../../components/addTier.js'
+
 export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+  addTier(req,res)
 }
